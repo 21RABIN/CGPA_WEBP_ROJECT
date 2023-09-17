31cadf0341be58abb07472012b44e03ef0a923a8
21RABIN <104710006+21RABIN@users.noreply.github.com>
2023-09-17 19:51:06 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,509 @@
+function calculate() {
+//semaster 1	
+  var css1c1,css1c2,css1c3,css1c4,css1c5,css1c6,css1c7,css1c8,css1tot;
+  var cscrs1c1,cscrs1c2,cscrs1c3,cscrs1c4,cscrs1c5,cscrs1c6,cscrs1c7,cscrs1c8;
+   
+//Get Grade Value
+   css1c1=document.getElementById('css1c1').value;
+   css1c2=document.getElementById('css1c2').value;
+   css1c3=document.getElementById('css1c3').value;
+   css1c4=document.getElementById('css1c4').value;
+   css1c5=document.getElementById('css1c5').value;
+   css1c6=document.getElementById('css1c6').value;
+   css1c7=document.getElementById('css1c7').value;
+   css1c8=document.getElementById('css1c8').value;
+//Get Credit Value Sem1
+   cscrs1c1=document.getElementById('cscrs1c1').value;
+   cscrs1c2=document.getElementById('cscrs1c2').value;
+   cscrs1c3=document.getElementById('cscrs1c3').value;
+   cscrs1c4=document.getElementById('cscrs1c4').value;
+   cscrs1c5=document.getElementById('cscrs1c5').value;
+   cscrs1c6=document.getElementById('cscrs1c6').value;
+   cscrs1c7=document.getElementById('cscrs1c7').value;
+   cscrs1c8=document.getElementById('cscrs1c8').value;
+
+css1tot=(css1c1*cscrs1c1)+(css1c2*cscrs1c2)+(css1c3*cscrs1c3)+(css1c4*cscrs1c4)+(css1c5*cscrs1c5)+(css1c6*cscrs1c6)+(css1c7*cscrs1c7)+(css1c8*cscrs1c8);
+css1avg=css1tot/18;
+css1avgs=css1avg.toFixed(3);
+
+//Semaster 2
+   var css2c1,css2c2,css2c3,css2c4,css2c5,css2c6,css2c7,css2c8,css2tot;
+   var cscrs2c1,cscrs2c2,cscrs2c3,cscrs2c4,cscrs2c5,cscrs2c6,cscrs2c7,cscrs2c8;
+
+   css2c1=document.getElementById('css2c1').value;
+   css2c2=document.getElementById('css2c2').value;
+   css2c3=document.getElementById('css2c3').value;
+   css2c4=document.getElementById('css2c4').value;
+   css2c5=document.getElementById('css2c5').value;
+   css2c6=document.getElementById('css2c6').value;
+   css2c7=document.getElementById('css2c7').value;
+   css2c8=document.getElementById('css2c8').value;
+//Get  Credit Value Sem2
+   cscrs2c1=document.getElementById('cscrs2c1').value;
+   cscrs2c2=document.getElementById('cscrs2c2').value;
+   cscrs2c3=document.getElementById('cscrs2c3').value;
+   cscrs2c4=document.getElementById('cscrs2c4').value;
+   cscrs2c5=document.getElementById('cscrs2c5').value;
+   cscrs2c6=document.getElementById('cscrs2c6').value;
+   cscrs2c7=document.getElementById('cscrs2c7').value;
+   cscrs2c8=document.getElementById('cscrs2c8').value;
+
+
+css2tot=(css2c1*cscrs2c1)+(css2c2*cscrs2c2)+(css2c3*cscrs2c3)+(css2c4*cscrs2c4)+(css2c5*cscrs2c5)+(css2c6*cscrs2c6)+(css2c7*cscrs2c7)+(css2c8*cscrs2c8);
+css2avg=css2tot/18;
+css2avgs=css2avg.toFixed(3);
+
+
+//Semaster 3
+   var css3c1,css3c2,css3c3,css3c4,css3c5,css3c6,css3c7,css3c8,css3c9,css3c10,css3tot;
+   var cscrs3c1,cscrs3c2,cscrs3c3,cscrs3c4,cscrs3c5,cscrs3c6,cscrs3c7,cscrs3c8,cscrs3c9,cscrs3c10;
+
+   css3c1=document.getElementById('css3c1').value;
+   css3c2=document.getElementById('css3c2').value;
+   css3c3=document.getElementById('css3c3').value;
+   css3c4=document.getElementById('css3c4').value;
+   css3c5=document.getElementById('css3c5').value;
+   css3c6=document.getElementById('css3c6').value;
+   css3c7=document.getElementById('css3c7').value;
+   css3c8=document.getElementById('css3c8').value;
+   css3c9=document.getElementById('css3c9').value;
+
+   //Get  Credit Value Sem2
+   cscrs3c1=document.getElementById('cscrs3c1').value;
+   cscrs3c2=document.getElementById('cscrs3c2').value;
+   cscrs3c3=document.getElementById('cscrs3c3').value;
+   cscrs3c4=document.getElementById('cscrs3c4').value;
+   cscrs3c5=document.getElementById('cscrs3c5').value;
+   cscrs3c6=document.getElementById('cscrs3c6').value;
+   cscrs3c7=document.getElementById('cscrs3c7').value;
+   cscrs3c8=document.getElementById('cscrs3c8').value;
+   cscrs3c9=document.getElementById('cscrs3c9').value;
+
+
+css3tot=(css3c1*cscrs3c1)+(css3c2*cscrs3c2)+(css3c3*cscrs3c3)+(css3c4*cscrs3c4)+(css3c5*cscrs3c5)+(css3c6*cscrs3c6)+(css3c7*cscrs3c7)+(css3c8*cscrs3c8)+(css3c9*cscrs3c9);
+css3avg=css3tot/22;
+css3avgs=css3avg.toFixed(3);
+
+
+//Semaster 4
+   var css4c1,css4c2,css4c3,css4c4,css4c5,css4c6,css4c7,css4c8,css4c9,css4c10,css4tot;
+   var cscrs4c1,cscrs4c2,cscrs4c3,cscrs4c4,cscrs4c5,cscrs4c6,cscrs4c7,cscrs4c8,cscrs4c9,cscrs4c10;
+
+   css4c1=document.getElementById('css4c1').value;
+   css4c2=document.getElementById('css4c2').value;
+   css4c3=document.getElementById('css4c3').value;
+   css4c4=document.getElementById('css4c4').value;
+   css4c5=document.getElementById('css4c5').value;
+   css4c6=document.getElementById('css4c6').value;
+   css4c7=document.getElementById('css4c7').value;
+   css4c8=document.getElementById('css4c8').value;
+   css4c9=document.getElementById('css4c9').value;
+   css4c10=document.getElementById('css4c10').value;
+
+//Get  Credit Value Sem2
+   cscrs4c1=document.getElementById('cscrs4c1').value;
+   cscrs4c2=document.getElementById('cscrs4c2').value;
+   cscrs4c3=document.getElementById('cscrs4c3').value;
+   cscrs4c4=document.getElementById('cscrs4c4').value;
+   cscrs4c5=document.getElementById('cscrs4c5').value;
+   cscrs4c6=document.getElementById('cscrs4c6').value;
+   cscrs4c7=document.getElementById('cscrs4c7').value;
+   cscrs4c8=document.getElementById('cscrs4c8').value;
+   cscrs4c9=document.getElementById('cscrs4c9').value;
+   cscrs4c10=document.getElementById('cscrs4c10').value;
+
+
+css4tot=(css4c1*cscrs4c1)+(css4c2*cscrs4c2)+(css4c3*cscrs4c3)+(css4c4*cscrs4c4)+(css4c5*cscrs4c5)+(css4c6*cscrs4c6)+(css4c7*cscrs4c7)+(css4c8*cscrs4c8)+(css4c9*cscrs4c9)+(css4c10*cscrs4c10);
+css4avg=css4tot/22;
+css4avgs=css4avg.toFixed(3);
+
+
+//Semaster 5
+   var css5c1,css5c2,css5c3,css5c4,css5c5,css5c6,css5c7,css5c8,css5c9,css5c10,css5tot;
+    var cscrs5c1,cscrs5c2,cscrs5c3,cscrs5c4,cscrs5c5,cscrs5c6,cscrs5c7,cscrs5c8,cscrs5c9,cscrs5c10;
+
+   css5c1=document.getElementById('css5c1').value;
+   css5c2=document.getElementById('css5c2').value;
+   css5c3=document.getElementById('css5c3').value;
+   css5c4=document.getElementById('css5c4').value;
+   css5c5=document.getElementById('css5c5').value;
+   css5c6=document.getElementById('css5c6').value;
+   css5c7=document.getElementById('css5c7').value;
+   css5c8=document.getElementById('css5c8').value;
+   css5c9=document.getElementById('css5c9').value;
+
+   //Get  Credit Value Sem2
+   cscrs5c1=document.getElementById('cscrs5c1').value;
+   cscrs5c2=document.getElementById('cscrs5c2').value;
+   cscrs5c3=document.getElementById('cscrs5c3').value;
+   cscrs5c4=document.getElementById('cscrs5c4').value;
+   cscrs5c5=document.getElementById('cscrs5c5').value;
+   cscrs5c6=document.getElementById('cscrs5c6').value;
+   cscrs5c7=document.getElementById('cscrs5c7').value;
+   cscrs5c8=document.getElementById('cscrs5c8').value;
+   cscrs5c9=document.getElementById('cscrs5c9').value;
+
+
+css5tot=(css5c1*cscrs5c1)+(css5c2*cscrs5c2)+(css5c3*cscrs5c3)+(css5c4*cscrs5c4)+(css5c5*cscrs5c5)+(css5c6*cscrs5c6)+(css5c7*cscrs5c7)+(css5c8*cscrs5c8)+(css5c9*cscrs5c9);
+css5avg=css5tot/22;
+css5avgs=css5avg.toFixed(3);
+
+
+//Semaster 6
+   var css6c1,css6c2,css6c3,css6c4,css6c5,css6c6,css6c7,css6c8,css6c9,css6c10,css6tot;
+     var cscrs6c1,cscrs6c2,cscrs6c3,cscrs6c4,cscrs6c5,cscrs6c6,cscrs6c7,cscrs6c8,cscrs6c9,cscrs6c10;
+
+   css6c1=document.getElementById('css6c1').value;
+   css6c2=document.getElementById('css6c2').value;
+   css6c3=document.getElementById('css6c3').value;
+   css6c4=document.getElementById('css6c4').value;
+   css6c5=document.getElementById('css6c5').value;
+   css6c6=document.getElementById('css6c6').value;
+   css6c7=document.getElementById('css6c7').value;
+   css6c8=document.getElementById('css6c8').value;
+   css6c9=document.getElementById('css6c9').value;
+   css6c10=document.getElementById('css6c10').value;
+   css6c11=document.getElementById('css6c11').value;
+
+
+    cscrs6c1=document.getElementById('cscrs6c1').value;
+   cscrs6c2=document.getElementById('cscrs6c2').value;
+   cscrs6c3=document.getElementById('cscrs6c3').value;
+   cscrs6c4=document.getElementById('cscrs6c4').value;
+   cscrs6c5=document.getElementById('cscrs6c5').value;
+   cscrs6c6=document.getElementById('cscrs6c6').value;
+   cscrs6c7=document.getElementById('cscrs6c7').value;
+   cscrs6c8=document.getElementById('cscrs6c8').value;
+   cscrs6c9=document.getElementById('cscrs6c9').value;
+   cscrs6c10=document.getElementById('cscrs6c10').value;
+   cscrs6c11=document.getElementById('cscrs6c11').value;
+
+css6tot=(css6c1*cscrs6c1)+(css6c2*cscrs6c2)+(css6c3*cscrs6c3)+(css6c4*cscrs6c4)+(css6c5*cscrs6c5)+(css6c6*cscrs6c6)+(css6c7*cscrs6c7)+(css6c8*cscrs6c8)+(css6c9*cscrs6c9)+(css6c10*cscrs6c10)+(css6c11*cscrs6c11);
+css6avg=css6tot/22;
+css6avgs=css6avg.toFixed(3);
+
+
+//Semaster 7
+   var css7c1,css7c2,css7c3,css7c4,css7c5,css7c6,css7c7,css7c8,css7c9,css7c10,css7tot;
+   var cscrs7c1,cscrs7c2,cscrs7c3,cscrs7c4,cscrs7c5,cscrs7c6,cscrs7c7,cscrs7c8,cscrs7c9,cscrs7c10;
+   css7c1=document.getElementById('css7c1').value;
+   css7c2=document.getElementById('css7c2').value;
+   css7c3=document.getElementById('css7c3').value;
+   css7c4=document.getElementById('css7c4').value;
+   css7c5=document.getElementById('css7c5').value;
+   css7c6=document.getElementById('css7c6').value;
+   css7c7=document.getElementById('css7c7').value;
+   css7c8=document.getElementById('css7c8').value;
+   css7c9=document.getElementById('css7c9').value;
+
+
+    cscrs7c1=document.getElementById('cscrs7c1').value;
+   cscrs7c2=document.getElementById('cscrs7c2').value;
+   cscrs7c3=document.getElementById('cscrs7c3').value;
+   cscrs7c4=document.getElementById('cscrs7c4').value;
+   cscrs7c5=document.getElementById('cscrs7c5').value;
+   cscrs7c6=document.getElementById('cscrs7c6').value;
+   cscrs7c7=document.getElementById('cscrs7c7').value;
+   cscrs7c8=document.getElementById('cscrs7c8').value;
+   cscrs7c9=document.getElementById('cscrs7c9').value;
+
+css7tot=(css7c1*cscrs7c1)+(css7c2*cscrs7c2)+(css7c3*cscrs7c3)+(css7c4*cscrs7c4)+(css7c5*cscrs7c5)+(css7c6*cscrs7c6)+(css7c7*cscrs7c7)+(css7c8*cscrs7c8)+(css7c9*cscrs7c9);
+css7avg=css7tot/24;
+css7avgs=css7avg.toFixed(3);
+
+
+//Semaster 8
+
+   var css4c1,css4c2,css4c3,css4tot;
+   var cscrs4c1,cscrs4c2,cscrs4c3;
+
+   css8c1=document.getElementById('css8c1').value;
+   css8c2=document.getElementById('css8c2').value;
+   css8c3=document.getElementById('css8c3').value;
+
+   cscrs8c1=document.getElementById('cscrs8c1').value;
+   cscrs8c2=document.getElementById('cscrs8c2').value;
+   cscrs8c3=document.getElementById('cscrs8c3').value;
+
+css8tot=(css2c1*cscrs8c1)+(css2c2*cscrs8c2)+(css2c3*cscrs8c3)
+css8avg=css4tot/12;
+css8avgs=css8avg.toFixed(3);
+
+
+var totalavg,totalavgg,totalavgs;
+
+var dis=document.querySelector('.result')
+
+const popupdisplay = document.querySelector('.popupdisplay');
+const popupbgr = document.querySelector('.popupbgr');
+const btnPopUpBox = document.querySelector('.btnPopUpBox');
+ var sem=document.getElementById('displaysem').value;
+
+dis.style.display='flex'
+    btnPopUpBox.style="flex";
+    popupdisplay.style.display="flex";
+    popupbgr.style.display="flex";
+
+if (sem==1) {
+
+   totalavg=css1avg/1;
+   totalavgs=totalavg.toFixed(3)
+   dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} \n Avarage CGPA: ${parseFloat(css1avgs)}`
+
+  
+
+
+}
+else if(sem==2)
+{
+   totalavg=css1avg+css2avg;
+   totalavgg=totalavg/2;
+   totalavgs=totalavgg.toFixed(3)
+
+ dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}\nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)}`
+}
+else if(sem==3)
+{
+     totalavg=css1avg+css2avg+css3avg;
+     totalavgg=totalavg/3
+     totalavgs=totalavgg.toFixed(3)
+     dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+     \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+     \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}`
+
+}
+else if(sem==4)
+{
+
+ totalavg=css1avg+css2avg+css3avg+css4avg;
+ totalavgg=totalavg/4
+ totalavgs=totalavgg.toFixed(3)
+ dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+ \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+ \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}
+ \nSem 4: ${parseFloat(css4avg).toFixed(3)}  CGPA: ${parseFloat(css4avgs)} `
+
+}
+else if(sem==5)
+{
+
+   totalavg=css1avg+css2avg+css3avg+css4avg+css5avg;
+   totalavgg=totalavg/5
+   totalavgs=totalavgg.toFixed(3)
+   dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+ \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+ \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}
+ \nSem 4: ${parseFloat(css4avg).toFixed(3)}  CGPA: ${parseFloat(css4avgs)}
+ \nSem 5: ${parseFloat(css5avg).toFixed(3)} CGPA: ${parseFloat(css5avgs)}`
+
+}
+else if(sem==6)
+{
+ totalavg=css1avg+css2avg+css3avg+css4avg+css5avg+css6avg;
+ totalavgg=totalavg/6
+   totalavgs=totalavgg.toFixed(3)
+    dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+ \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+ \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}
+ \nSem 4: ${parseFloat(css4avg).toFixed(3)}  CGPA: ${parseFloat(css4avgs)}
+ \nSem 5: ${parseFloat(css5avg).toFixed(3)} CGPA: ${parseFloat(css5avgs)}
+ \nSem 6: ${parseFloat(css6avg).toFixed(3)}  CGPA: ${parseFloat(css6avgs)}`
+
+}
+else if(sem==7)
+{
+
+ totalavg=css1avg+css2avg+css3avg+css4avg+css5avg+css7avg;
+ totalavgg=totalavg/7
+   totalavgs=totalavgg.toFixed(3)
+   dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+ \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+ \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}
+ \nSem 4: ${parseFloat(css4avg).toFixed(3)}  CGPA: ${parseFloat(css4avgs)}
+ \nSem 5: ${parseFloat(css5avg).toFixed(3)} CGPA: ${parseFloat(css5avgs)}
+ \nSem 6: ${parseFloat(css6avg).toFixed(3)}  CGPA: ${parseFloat(css6avgs)}
+ \nSem 7: ${parseFloat(css7avg).toFixed(3)} CGPA: ${parseFloat(css7avgs)}`
+
+}
+else if(sem==8)
+{
+       totalavg=css1avg+css2avg+css3avg+css4avg+css5avg+css6avg+css7avg+css8avg;
+       totalavgg=totalavg/8
+   totalavgs=totalavgg.toFixed(3)
+   dis.innerText = `\nSem 1: ${parseFloat(css1avg).toFixed(3)} CGPA: ${parseFloat(css1avgs)}
+ \nSem 2: ${parseFloat(css2avg).toFixed(3)}  CGPA: ${parseFloat(css2avgs)} 
+ \nSem 3: ${parseFloat(css3avg).toFixed(3)}  CGPA: ${parseFloat(css3avgs)}
+ \nSem 4: ${parseFloat(css4avg).toFixed(3)}  CGPA: ${parseFloat(css4avgs)}
+ \nSem 5: ${parseFloat(css5avg).toFixed(3)} CGPA: ${parseFloat(css5avgs)}
+ \nSem 6: ${parseFloat(css6avg).toFixed(3)}  CGPA: ${parseFloat(css6avgs)}
+ \nSem 7: ${parseFloat(css7avg).toFixed(3)} CGPA: ${parseFloat(css7avgs)}
+ \nSem 8: ${parseFloat(css8avg).toFixed(3)}  CGPA: ${parseFloat(css8avgs)}`;
+  
+
+}
+else
+{
+   alert('Plese Select Semaster')
+   
+}
+
+// --------------------------------------------------------------------------
+
+}
+
+//What Semastr Display
+
+function displaysemester() {
+    
+    var sem,sem1,sem2,sem3,sem4,sem5,sem6,sem7,sem8;
+ sem=document.getElementById('displaysem').value;
+ holesem=document.querySelectorAll('.container')
+ sem1=document.querySelector('.container1')
+ sem2=document.querySelector('.container2')
+ sem3=document.querySelector('.container3')
+ sem4=document.querySelector('.container4')
+ sem5=document.querySelector('.container5')
+ sem6=document.querySelector('.container6')
+ sem7=document.querySelector('.container7')
+ sem8=document.querySelector('.container8')
+if (sem==1) {
+         for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+      sem1.style.display='block'
+   }
+}
+else if(sem==2)
+{
+    for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+      sem1.style.display='block'
+      sem2.style.display='block'
+   }
+
+}
+else if(sem==3)
+{
+
+   for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+       sem1.style.display='block'
+      sem2.style.display='block'
+      sem3.style.display='block'
+   }
+
+}
+else if(sem==4)
+{
+     for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+      sem1.style.display='block'
+      sem2.style.display='block'
+      sem3.style.display='block'
+      sem4.style.display='block'
+   }
+}
+else if(sem==5)
+{
+   for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+       sem1.style.display='block'
+      sem2.style.display='block'
+      sem3.style.display='block'
+      sem4.style.display='block'
+      sem5.style.display='block'
+   }
+
+}
+else if(sem==6)
+{
+   for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+       sem1.style.display='block'
+      sem2.style.display='block'
+      sem3.style.display='block'
+      sem4.style.display='block'
+      sem5.style.display='block'
+      sem6.style.display='block'
+   }
+
+}
+else if(sem==7)
+{
+   for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='none'
+             sem1.style.display='block'
+      sem2.style.display='block'
+      sem3.style.display='block'
+      sem4.style.display='block'
+      sem5.style.display='block'
+      sem6.style.display='block'
+      sem7.style.display='block'
+   }
+
+}
+else if(sem==8)
+{
+   for(var i=0;i<holesem.length;i++)
+   {
+      holesem[i].style.display='block'
+   }
+
+}
+
+
+}
+
+
+function Closebtn() {
+
+  const popupbgr=document.querySelector('.popupbgr')
+
+          popupbgr.style.display='none'
+  
+}
+
+
+
+/*-------------------------------------------------------------------------------------------*/
+
+function sem2021() {
+
+  const sem2021=document.querySelector('.sem2021')
+  const sem2022=document.querySelector('.sem2022')
+  const mainmenu=document.querySelector('.mainmenu')
+   sem2021.style.display='block'
+   sem2022.style.display='none'
+   mainmenu.style.display='none'
+
+   // body...
+}
+function sem2022() {
+
+  const sem2021=document.querySelector('.sem2021')
+  const sem2022=document.querySelector('.sem2022')
+  const mainmenu=document.querySelector('.mainmenu')
+
+   sem2022.style.display='block'
+   sem2021.style.display='none'
+    mainmenu.style.display='none'
+   // body...
+}
+
+function regulation() {
+  const sem2021=document.querySelector('.sem2021')
+  const sem2022=document.querySelector('.sem2022')
+  const mainmenu=document.querySelector('.mainmenu')
+  const regulation=document.querySelector('.regulation')
+
+  regulation.style.display='block'
+   sem2022.style.display='none'
+   sem2021.style.display='none'
+    mainmenu.style.display='none'
+}
